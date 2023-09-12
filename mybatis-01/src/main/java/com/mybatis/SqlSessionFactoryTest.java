fe2cae080ef52b954c71ee30d9749b9b60ed525d
pll <panleilei@kedacom.com>
2023-09-12 19:25:19 +0800

    feat: create repository

---
+++ mybatis-01/src/main/java/com/mybatis/SqlSessionFactoryTest.java
@@ -0,0 +1,17 @@
+package com.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * @author ：panleilei
+ * @description
+ * @modified By：
+ * @version:
+ * @date ：Created in 2023/9/12 17:28
+ */
+public class SqlSessionFactoryTest {
+    public static void main(String[] args) {
+
+
+    }
+}
